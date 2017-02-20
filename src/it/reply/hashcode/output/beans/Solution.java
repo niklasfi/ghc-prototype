575c65b0216ae36ab026aa90efcf3acec60e1ecf
Niklas Fischer <n.fischer@reply.de>
2017-02-20 11:30:37 +0100

    add data structure for sln

--- src/it/reply/hashcode/output/beans/Solution.java
+++ src/it/reply/hashcode/output/beans/Solution.java
@@ -4,11 +4,30 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.reply.hashcode.Server;
+import it.reply.hashcode.input.beans.Problem;
+
+class RunEncodeNode {
+	public int start;
+	public int length;
+	public Server server;
+	public int pool;
+}
+
+class Row{
+	public ArrayList<RunEncodeNode> slots;
+	public ArrayList<Integer> poolCapacity;
+}
+
 /**
  * @author d.cavassa
  *
  */
 @SuppressWarnings("serial")
 public class Solution {
+	public Problem problem;
+	public ArrayList<Row> rows;
+	public ArrayList<Server> remainingServers;
+	
 	
 }//SectionBean
