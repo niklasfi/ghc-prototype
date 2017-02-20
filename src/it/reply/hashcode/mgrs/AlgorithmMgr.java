9d3226a75f98b020d03c0007360c5cb190a41d4f
m.manino <m.manino@CLU12182.replynet.prv>
2017-02-20 13:19:00 +0100

    dsa

--- src/it/reply/hashcode/mgrs/AlgorithmMgr.java
+++ src/it/reply/hashcode/mgrs/AlgorithmMgr.java
@@ -18,71 +18,70 @@ import it.reply.hashcode.output.beans.Solution;
  */
 
 public class AlgorithmMgr implements Runnable {
-	
+
 	private final Problem problem;
 	private int optimalScore = 0;
-	private ScoreMgr g_scoreMgr = null; 
+	private ScoreMgr g_scoreMgr = null;
 	private final Random randomGenerator = new Random(System.currentTimeMillis());
 	private Solution best;
-	
-	public AlgorithmMgr(File inData) throws IOException{
+
+	public AlgorithmMgr(File inData) throws IOException {
 		problem = new Problem(inData);
-		g_scoreMgr = new ScoreMgr(); 
+		g_scoreMgr = new ScoreMgr();
 		best = problem.emptySolution();
 	}
-	
+
 	public synchronized Random getRandom() {
 		return new Random(randomGenerator.nextInt());
 	}
-	
+
 	// Generates solutions
 	public void run() {
-		//TODO
+		// TODO
 		Random r = getRandom();
 		Solution sln = destroy(r, getBestSolution(), 0.3f);
-		
-		//sort remaining servers
+
+		// sort remaining servers
 		Comparator<Server> compareServers = (s1, s2) -> -Integer.compare(s1.capacity / s1.size, s2.capacity / s2.size);
 		sln.remainingServers.sort(compareServers);
-		
+
 		boolean stepSuccess = true;
-		mainLoop:while(stepSuccess && sln.remainingServers.size() > 0){
+		mainLoop: while (stepSuccess && sln.remainingServers.size() > 0) {
 			stepSuccess = false;
 
-			//we're working on currentPool
-			for(int currentPool = 0; currentPool < problem.poolNumber; ++currentPool){
-				//sort rows by relevance
+			// we're working on currentPool
+			for (int currentPool = 0; currentPool < problem.poolNumber; ++currentPool) {
+				// sort rows by relevance
 				Integer[] rows = new Integer[problem.rows.size()];
-				for(int i = 0; i < rows.length; ++i){
+				for (int i = 0; i < rows.length; ++i) {
 					rows[i] = i;
 				}
-				
+
 				final int currentPoolFinal = currentPool;
-	 			Comparator<Integer> compareRows = (r1, r2) -> Integer.compare(
-						sln.rows.get(r1).poolCapacity.get(currentPoolFinal), 
-						sln.rows.get(r2).poolCapacity.get(currentPoolFinal)
-				);
+				Comparator<Integer> compareRows = (r1, r2) -> Integer.compare(
+						sln.rows.get(r1).poolCapacity.get(currentPoolFinal),
+						sln.rows.get(r2).poolCapacity.get(currentPoolFinal));
 				Arrays.sort(rows, compareRows);
-				for(int rowIndex = 0; rowIndex < rows.length; ++rowIndex){
-					//decide which segment to work on
+				for (int rowIndex = 0; rowIndex < rows.length; ++rowIndex) {
+					// decide which segment to work on
 					Row row = sln.rows.get(rows[rowIndex]);
-					
-					//sort seggments by relevance
+
+					// sort seggments by relevance
 					Integer[] segments = new Integer[row.segments.size()];
-					for(int i = 0; i < segments.length; ++i){
+					for (int i = 0; i < segments.length; ++i) {
 						segments[i] = i;
 					}
-					Comparator<Integer> compareSegments = (s1, s2) -> -Integer.compare(
-							row.segments.get(s1).sizeRemaining, row.segments.get(s2).sizeRemaining);
+					Comparator<Integer> compareSegments = (s1, s2) -> -Integer
+							.compare(row.segments.get(s1).sizeRemaining, row.segments.get(s2).sizeRemaining);
 					Arrays.sort(segments, compareSegments);
-					
-					for(int segmentIndex = 0; segmentIndex < segments.length; ++segmentIndex){
+
+					for (int segmentIndex = 0; segmentIndex < segments.length; ++segmentIndex) {
 						Segment segment = row.segments.get(segments[segmentIndex]);
-						
+
 						// try to find a fitting server
-						for(int serverIndex = 0; serverIndex < sln.remainingServers.size(); ++serverIndex){
+						for (int serverIndex = 0; serverIndex < sln.remainingServers.size(); ++serverIndex) {
 							Server server = sln.remainingServers.get(serverIndex);
-							if(server.size <= segment.sizeRemaining){
+							if (server.size <= segment.sizeRemaining) {
 								segment.addServer(server, currentPool);
 								stepSuccess = true;
 								continue mainLoop;
@@ -92,26 +91,33 @@ public class AlgorithmMgr implements Runnable {
 				}
 			}
 		}
-	}//generateNextSolution
-	
+
+		int score = g_scoreMgr.evaluate(sln);
+		if (score >= optimalScore) {
+			synchronized (this) {
+				optimalScore = score;
+				best = sln;
+			}
+		}
+
+	}// generateNextSolution
 
 	public synchronized void setBestSolution(Solution solution) {
 		best = solution;
 	}
-	
+
 	private Solution destroy(Random r, Solution old, float percent) {
 		Solution sol = new Solution(old);
-		for(int n = (int)(percent * sol.problem.servers.size()); n > 0; --n){
+		for (int n = (int) (percent * sol.problem.servers.size()); n > 0; --n) {
 			Row row = sol.rows.get(r.nextInt(sol.rows.size()));
 			Segment s = row.segments.get(r.nextInt(row.segments.size()));
 			sol.remainingServers.add(s.removeServer(r.nextInt(s.server.size())));
 		}
 		return sol;
 	}
-	
+
 	public Solution getBestSolution() {
 		return new Solution(best);
-	}//getSolution
-	
-}//Class AlgorithmMgr
+	}// getSolution
 
+}// Class AlgorithmMgr
