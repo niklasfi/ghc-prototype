698978c35533ff26f2adb49129cd5e376c8ab899
unknown <m.queirolo@BLU11406.replynet.prv>
2017-02-21 09:58:25 +0100

    '.'

--- src/it/reply/hashcode/SolutionMgr.java
+++ src/it/reply/hashcode/SolutionMgr.java
@@ -15,10 +15,10 @@ public class SolutionMgr {
 
 //		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/3e68ab86-b216-40f1-b7ad-41166b2b0ffe.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
 //		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/5c765651-ed73-4180-90d5-66a43373be2f.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
-//		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/70ab7a98-48c8-4bb5-a1bc-628974d27002.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
+		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/70ab7a98-48c8-4bb5-a1bc-628974d27002.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
 //		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/92c5431d-4bfd-4dc7-8a7a-1e2ad77e9b6c.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
 //		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/098caacb-e39e-469b-8f91-6521567740ad.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
-		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/ebcbe09d-a9e8-4c8c-ae23-761e44824c7a.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
+//		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/ebcbe09d-a9e8-4c8c-ae23-761e44824c7a.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
 //		args = new String[] { "C:/Users/M.Queirolo/Desktop/hashcode/input/dcexample.in", "C:/Users/M.Queirolo/Desktop/hashcode/output/" };
 		
 		if(args == null || args.length == 0){
